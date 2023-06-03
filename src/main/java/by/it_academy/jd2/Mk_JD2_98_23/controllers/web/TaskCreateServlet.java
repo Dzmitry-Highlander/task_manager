c56019c11293df1d36795b80ca2ab3c6df365ca5
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-03 15:33:16 +0300

    add: ObjectMapperFactory.java, TaskCreateServlet.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/TaskCreateServlet.java
@@ -0,0 +1,33 @@
+package by.it_academy.jd2.Mk_JD2_98_23.controllers.web;
+
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.ITaskCreateService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.ObjectMapperFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.TaskServiceFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/api/create")
+public class TaskCreateServlet extends HttpServlet {
+    private final ITaskCreateService taskCreateService;
+    private final ObjectMapper objectMapper;
+
+    public TaskCreateServlet() {
+        this.taskCreateService = TaskServiceFactory.getInstance();
+        this.objectMapper = ObjectMapperFactory.getInstance();
+        this.objectMapper.findAndRegisterModules();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+
+        writer.write(objectMapper.writeValueAsString(taskCreateService.get()));
+    }
+}
