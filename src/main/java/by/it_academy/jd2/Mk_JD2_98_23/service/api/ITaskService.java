bca8ff953dfa90f5578ea23b32809d4605f67191
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-03 14:21:28 +0300

    add: ICRUDService.java, ITaskService.java, ServiceException.java, TaskService.java, TaskServiceFactory.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/ITaskService.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.api;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.TaskCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.TaskDTO;
+
+public interface ITaskService extends ICRUDService<TaskCreateDTO, TaskDTO> {
+}
---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/factory/TaskServiceFactory.java
@@ -0,0 +1,20 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.factory;
+
+import by.it_academy.jd2.Mk_JD2_98_23.dao.db.factory.TaskJDBCDaoFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.service.TaskService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.ITaskService;
+
+public class TaskServiceFactory {
+    private static volatile ITaskService instance;
+
+    private TaskServiceFactory() {
+    }
+
+    public static ITaskService getInstance() {
+        if (instance == null) {
+            synchronized (TaskServiceFactory.class) {
+                instance = new TaskService(TaskJDBCDaoFactory.getInstance());
+            }
+        }
+    }
+}
