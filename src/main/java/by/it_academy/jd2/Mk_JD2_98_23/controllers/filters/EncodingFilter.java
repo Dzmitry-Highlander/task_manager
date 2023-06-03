2cd83ca4c385d6ee1d325e777a471279d90f3821
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-03 11:38:39 +0300

    feat: EncodingFilter.doFilter, EncodingFilter.init

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/filters/EncodingFilter.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/filters/EncodingFilter.java
@@ -1,4 +1,33 @@
 package by.it_academy.jd2.Mk_JD2_98_23.controllers.filters;
 
-public class EncodingFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("*")
+public class EncodingFilter implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        encoding = filterConfig.getInitParameter("encoding");
+
+        if (encoding == null) {
+            encoding = "UTF-8";
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
 }
