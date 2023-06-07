3ac248f620b628806f0c592b922657515ed3ccd1
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-07 20:12:22 +0300

    add: Sort.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/enums/Sort.java
@@ -0,0 +1,22 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service.enums;
+
+public enum Sort {
+    ID_ASC ("task_id ASC"),
+    ID_DESC ("task_id DESC"),
+    HEADER_ASC ("header ASC"),
+    HEADER_DESC ("header DESC"),
+    DEADLINE_ASC ("deadline ASC"),
+    DEADLINE_DESC ("deadline DESC"),
+    STATUS_ASC ("status ASC"),
+    STATUS_DESC ("status DESC");
+
+    private String sort;
+
+    Sort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+}
