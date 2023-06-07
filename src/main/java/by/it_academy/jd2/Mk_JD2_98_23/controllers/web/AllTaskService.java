e2a189ba4132efaec4d70de74c5917caae9b518c
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-07 19:21:02 +0300

    add: AllTaskService.java

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/AllTaskService.java
@@ -0,0 +1,38 @@
+package by.it_academy.jd2.Mk_JD2_98_23.controllers.web;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.TaskDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.ITaskService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.ObjectMapperFactory;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.TaskServiceFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/api/task/all")
+public class AllTaskService extends HttpServlet {
+    private final ITaskService taskService;
+    private final ObjectMapper objectMapper;
+
+    public AllTaskService() {
+        this.taskService = TaskServiceFactory.getInstance();
+        this.objectMapper = ObjectMapperFactory.getInstance();
+        this.objectMapper.findAndRegisterModules();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+
+        PrintWriter writer = resp.getWriter();
+        List<TaskDTO> dtos = taskService.get();
+
+        writer.write(objectMapper.writeValueAsString(dtos));
+    }
+}
