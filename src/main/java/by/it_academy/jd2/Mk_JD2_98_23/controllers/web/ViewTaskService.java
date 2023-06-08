2aac7cebc1732b059ecb159084e7774ba306491f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-06-08 19:23:00 +0300

    refactor: ViewTaskService.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/ViewTaskService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/ViewTaskService.java
@@ -18,7 +18,6 @@ import java.util.LinkedHashMap;
 @WebServlet("/api/task/view")
 public class ViewTaskService extends HttpServlet {
     private static final String SORT = "sort";
-    private static final String FILTER_HEADER = "header";
     private final ITaskService taskService;
     private final ObjectMapper objectMapper;
 
@@ -31,22 +30,9 @@ public class ViewTaskService extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
-        String sort = null;
-        String header = null;
 
-        if (req.getParameter(SORT).isEmpty()) {
-            sort = req.getParameter(SORT);
-        } else if (req.getParameter(FILTER_HEADER).isEmpty()) {
-            header = req.getParameter(FILTER_HEADER);
-        }
         PrintWriter writer = resp.getWriter();
         LinkedHashMap<Long, TaskDTO> dtos;
 
-        assert sort != null;
-        int sortParsed = Integer.parseInt(sort);
-
-        dtos = taskService.getSorted(sortParsed);
-
-        writer.write(objectMapper.writeValueAsString(dtos.values()));
     }
 }
